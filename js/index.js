05cebd4682ddcd3f833f64eb755b6cc214ceaadb
Anissa Nishioka <ani.nishioka@berkeley.edu>
2018-06-17 14:52:59 -0700

    travel gallery added

--- js/index.js
+++ js/index.js
@@ -21,14 +21,15 @@ function setOverlay() {
 function center(pseudo) {
   photoCatg = $(pseudo).parent();
   const left = Math.max(0, ($(".photo.content").innerWidth() - photoCatg.width()) / 2);
+  //const left = $(".lrgPhoto").position().left;
   offset = left - photoCatg.position().left;
   $(pseudo).animate({
     left: `+=${offset}`
-  }, 900);
+  }, 1000);
 }
 
 //display photo when thumbnail clicked
-function display(key) {
+function display() {
   const newPhoto = $(this).attr("data-large");
   $("img.lrgPhoto").css({"opacity": "0"});
   $("img.lrgPhoto").attr({"src": newPhoto});
@@ -44,11 +45,19 @@ function keyDisplay(key) {
   var nextPhoto;
   if (key == 39) {
     nextTb = $(currTb).parent().next().children(':first');
-    nextPhoto = $(nextTb).attr("data-large");
+    //if at last photo, loop back to first
+    if (nextTb.length === 0) {
+      nextTb = $(".hover").children(':first').children();
+      console.log(nextTb);
+    }
   } else {
     nextTb = $(currTb).parent().prev().children(':first');
-    nextPhoto = $(nextTb).attr("data-large");
+    //if at first photo, loop back to last
+    if (nextTb.length === 0) {
+      nextTb = $(".hover").children(':last-child').children()
+    }
   }
+  nextPhoto = $(nextTb).attr("data-large");
   $("img.lrgPhoto").css({"opacity": "0"});
   $("img.lrgPhoto").attr({"src": nextPhoto});
   $("img.lrgPhoto").css({"opacity": "1"});
@@ -61,14 +70,15 @@ function photoTransition(photoLink) {
   const pseudo = $(photoLink).siblings(".pseudo");
 
   //isolate chosen category
-  photoCatgSibs.animate({opacity: 0}, 300);
+  $('.photoCatg').animate({opacity: 0}, 700, 'linear');
+  //photoCatgSibs.animate({opacity: 0}, 300);
   $(".pg.photo").css({overflow: "hidden"});
 
-  $(pseudo).css({opacity: 1});
+  //$(pseudo).css({opacity: 1});
 
-  //remove overlay
+  /*//remove overlay
   $(photoCatg).children(".photoLink").css({opacity: 0});
-  $(photoCatg).children(".photo").css({opacity: 0});
+  $(photoCatg).children(".photo").css({opacity: 0});*/ //glide transition
 
   //make pg and content fit window
   $(".content.photo").css({
@@ -76,25 +86,51 @@ function photoTransition(photoLink) {
     width: "80%"
   });
 
-  //transition pseudo to center
-  center(pseudo);
+  /*$(".galleryWrap").show();
+  $(".galleryWrap").css({display: 'flex'}); experimenting*/
+
+  /*//transition pseudo to center
+  center(pseudo);*/ //glide transition
 
   setTimeout(function() {
     //make pseudo transparent and move back to position of photoCatg
-    $(pseudo).css({opacity: 0});
+    //$(pseudo).css({opacity: 0}); glide transition
     setOverlay();
 
     //replace photo nav page with gallery
     $(".content.photo").hide();
-    $(".galleryWrap").css({display: 'flex'});
+    $(".galleryWrap").css({display: 'flex'}); //just have it like this from the start!!!!
     $(".galleryWrap").show();
+    //$(".galleryWrap").css({opacity: 1});
+    $(".galleryWrap").delay(700).animate({opacity: 1}, 700, 'linear');
 
     //set up for when go back to photo catg page
-    $(pseudo).css({opacity: 1});
+    /*$(pseudo).css({opacity: 1});
     $(photoCatg).children(".photoLink").css({opacity: 1});
-    $(photoCatg).children(".photo").css({opacity: 1});
-  }, 1000);
+    $(photoCatg).children(".photo").css({opacity: 1});*/  //jk remove bc want to fadein
+    $('.photoCatg').css({opacity: 1});
+
+    $('ul.hover').delay(1700).animate({opacity: 1}, 800, 'linear');
+    $('ul.hover').delay(1100).animate({opacity: 0}, 700, 'linear');
+  }, 1100);
 }
+
+//photo gallery back to photoCatg
+function transitionBack() {
+  $('.galleryWrap').animate({opacity: 0}, 700, 'linear');
+
+  setTimeout(function() {
+    $(".content.photo").hide();
+    /*$(".content.photo").show();
+    $('.photoCatg').animate({opacity: 1}, 700, 'linear');*/
+    $('.content.photo').load(document.URL +  ' .content.photo');
+    $(".content.photo").show()
+  }, 1200);
+
+}
+
+
+
 
 $(document).ready(function() {
   //align overlay on load
@@ -133,6 +169,9 @@ $(document).ready(function() {
     }
     keyDisplay(key);
   });
+
+  //back to photoCatg when X clicked
+  $('.exit').click(transitionBack);
 
   //$(".photoLink").click(photoTransition);
 
