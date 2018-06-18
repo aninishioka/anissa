c96d38f77fa05623b7a14556cfd0b7800d1c69a7
Anissa Nishioka <ani.nishioka@berkeley.edu>
2018-06-18 15:03:42 -0700

    add people gallery

--- js/index.js
+++ js/index.js
@@ -37,8 +37,8 @@ function display() {
   //animate({opacity: 1});
 }
 
-function keyDisplay(key) {
-  var photo = $('img.lrgPhoto').attr('src');
+function keyDisplay(key, gallery) {
+  var photo = $(`img.lrgPhoto[data-catg='${gallery}']`).attr('src');
   photo = photo.replace('assets/', '');
   currTb = $(`img[src="thumbnails/${photo}"]`);
   var nextTb;
@@ -47,41 +47,37 @@ function keyDisplay(key) {
     nextTb = $(currTb).parent().next().children(':first');
     //if at last photo, loop back to first
     if (nextTb.length === 0) {
-      nextTb = $(".hover").children(':first').children();
-      console.log(nextTb);
+      nextTb = $(`.hover[data-catg='${gallery}']`).children(':first').children();
     }
   } else {
     nextTb = $(currTb).parent().prev().children(':first');
     //if at first photo, loop back to last
     if (nextTb.length === 0) {
-      nextTb = $(".hover").children(':last-child').children()
+      nextTb = $(`.hover[data-catg='${gallery}']`).children(':last-child').children()
     }
   }
   nextPhoto = $(nextTb).attr("data-large");
-  $("img.lrgPhoto").css({"opacity": "0"});
-  $("img.lrgPhoto").attr({"src": nextPhoto});
-  $("img.lrgPhoto").css({"opacity": "1"});
+  $(`img.lrgPhoto[data-catg='${gallery}']`).css({"opacity": "0"});
+  $(`img.lrgPhoto[data-catg='${gallery}']`).attr({"src": nextPhoto});
+  $(`img.lrgPhoto[data-catg='${gallery}']`).css({"opacity": "1"});
 }
 
 //transition between photo pages
 function photoTransition(photoLink) {
   const photoCatg = $(photoLink).parent();
   const photoCatgSibs = $(photoCatg).siblings();
+  const catgName = $(photoCatg).attr('data-catg');
   //const pseudo = $(photoLink).siblings(".pseudo");
 
   //isolate chosen category
   $('.photoCatg').animate({opacity: 0}, 700, 'linear');
-  //photoCatgSibs.animate({opacity: 0}, 300);
-  //$(".pg.photo").css({overflow: "hidden"});
+  //photoCatgSibs.animate({opacity: 0}, 300); glide
 
-  //$(pseudo).css({opacity: 1});
+  //$(pseudo).css({opacity: 1}); glide
 
   /*//remove overlay
   $(photoCatg).children(".photoLink").css({opacity: 0});
   $(photoCatg).children(".photo").css({opacity: 0});*/ //glide transition
-
-  /*$(".galleryWrap").show();
-  $(".galleryWrap").css({display: 'flex'}); experimenting*/
 
   /*//transition pseudo to center
   center(pseudo);*/ //glide transition
@@ -93,16 +89,18 @@ function photoTransition(photoLink) {
 
     //replace photo nav page with gallery
     $(".content.photo").hide();
-    $(".galleryWrap").css({display: 'flex'}); //just have it like this from the start!!!!
+    const currGallery = $(`.galleryWrap[data-catg='${catgName}']`);
+    $(currGallery).css({display: 'flex'});
+    $(currGallery).show();
+    $(currGallery).delay(700).animate({opacity: 1}, 700, 'linear');
+    /*$(".galleryWrap").css({display: 'flex'}); //just have it like this from the start!!!!
     $(".galleryWrap").show();
-    //$(".galleryWrap").css({opacity: 1});
-    $(".galleryWrap").delay(700).animate({opacity: 1}, 700, 'linear');
+    $(".galleryWrap").delay(700).animate({opacity: 1}, 700, 'linear');*/
 
     //set up for when go back to photo catg page
     /*$(pseudo).css({opacity: 1});
     $(photoCatg).children(".photoLink").css({opacity: 1});
     $(photoCatg).children(".photo").css({opacity: 1});*/  //jk remove bc want to fadein
-    //$('.photoCatg').css({opacity: 1});
 
     $('ul.hover').delay(1700).animate({opacity: 1}, 800, 'linear');
     $('ul.hover').delay(1100).animate({opacity: 0}, 700, 'linear');
@@ -111,13 +109,13 @@ function photoTransition(photoLink) {
 
 //photo gallery back to photoCatg
 function transitionBack() {
+  console.log('hi');
   $('.galleryWrap').animate({opacity: 0}, 700, 'linear');
 
   setTimeout(function() {
     $(".galleryWrap").hide();
     $(".content.photo").show();
     $('.photoCatg').animate({opacity: 1}, 700, 'linear');
-    //$('.content.photo').load(document.URL +  ' .content.photo');
     $(".content.photo").show();
   }, 1200);
 
@@ -142,12 +140,11 @@ $(document).ready(function() {
   });
 
   //photo category link transition
-  var galleryOpen = false;
   var currGallery;
   $(".photoLink").click(function() {
     galleryOpen = true;
     let photoCatg = $(this).parent();
-    currGallery = $(photoCatg);
+    currGallery = $(photoCatg).attr('data-catg');
     //photoTransition(photoCatg);
     photoTransition(this);
   });
@@ -161,7 +158,7 @@ $(document).ready(function() {
     if (!(key == 37 ^ key == 39)) {
       return;
     }
-    keyDisplay(key);
+    keyDisplay(key, currGallery);
   });
 
   //back to photoCatg when X clicked
